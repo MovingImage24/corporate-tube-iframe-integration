bf502a02158393d687b862da1a45b767d0fe1636
Vukasin Nikodijevic <vukasin.nikodijevic@easycollab.net>
2018-11-14 13:08:54 +0100

    Add libs and update readme.

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# corporate-tube-iframe-integration
-Iframe integration library repo
+# iFrame Integration
+
+## Parent page (_page which integrates ct iframe_)
+
+* We provide an encapsulated integration through the client library `embed.js` 
+* Files:
+```
+dist/
+  - _languageData.js
+  - guessLanguage.js
+  - embed.js
+```
+* Parent page should include the files in preserved order into the header section.
+* Parent page should provide a DIV element in which the iframe will be placed. 
+* DIV element **must** have `id` which will be later on configured.
+
+## Example of implementation:
+
+### URL Structure
+
+Suggested integrator web page URL schema:
+> `https://example.com/videos` + `/channel/83281271/hr/video/23492348`
+
+The first part of URL `https://example.com/videos` is part under control of the parent page which integrates the CT iFrame - let's call it from now on `Parent frame`.
+
+The second part of the URL `/channel/83281271/hr/video/23492348` will be proxied to iFrame as CT resource allocation url. Let's call it from now on `Child frame`.
+
+#### Important !!!!
+> _The url `https://example.com/videos/channel/83281271/hr/video/23492348` in integration portal must always load `baseUrl` `https://example.com/videos` and DO NOT interpret right side of the URL `/channel/83281271/hr/video/23492348`. Right side of url should be completely ignored by routing. 
+For applications with routed systems it should not be a problem `(regex example: /^\/videos.*$/)`._
+
+#### How Frames should looks in code:
+
+```
+<!-- This is Parent frame-->
+<html lang="en">
+    <body>
+        <div id="ct-frame-wrapper">
+            <!-- This is CT Child frame-->
+            <iframe src="......"></iframe>
+        </div>
+    </body>
+</html>
+```
+
+<div class="page" />
+
+#### Integration codding part
+```html
+<html lang="en">
+    <head>
+        <script src="/static/_languageData.js"></script>
+        <script src="/static/guessLanguage.js"></script>
+        <script src="/static/embed.js"></script>
+        <script>
+            document.addEventListener("DOMContentLoaded", function () {
+                var baseUrl = "https://example.com/videos";
+                var ctUrl = "https://example.corporate.tube";
+                var ctEmbedded = new CtEmbedded(
+                    {
+                        baseUrl: baseUrl, 
+                        wrapperId: "ct-frame-wrapper"
+                    },
+                    ctUrl
+                );
+
+                ctEmbedded.subscribe(function(exchangeMessage) {
+                    // This line add changed ct iframe route to parent website integration page
+                    // In the other words end user will be able to copy url and send to someone in order to share ct deep linkable content
+                    history.replaceState(history.state, "iframe", baseUrl + exchangeMessage.routeChanged);
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <div id="ct-frame-wrapper"></div>
+    </body>
+</html>
+```
+
+<div class="page" />
+
+**How it works**
+
+* `embed.js` 
+    > is the library to be provided to the customer who is going to integrate Corporate Tube into their system as iFrame
+
+* `CtEmbedded` 
+    > stands for Corporate Tube Embedded and it is the main object which will create integration.
+
+    **_Important:_**
+    > _Don't create iframe html element manually - it will be placed by library into div with `id` `===` `wrapperId` config parameter._
+    
+    _Configuration options_:
+    
+    * `baseUrl` _`mandatory`_ - Must be provided and it stands for the url of `Parent Frame` page
+    
+    * `wrapperId` _`optional`_ - id of wrapper DIV which will hold iframe element. `Default: "ct-frame-wrapper"`
+
+* `<div id="ct-frame-wrapper"></div>` 
+    > is wrapper div where the iframe is going to be placed.
+
+* `ctEmbedded.subscribe` 
+    > method will receive exchange messages which Corporate Tube `Child Frame` is sending to `Parent Frame`. Usually, messages will contain `newCtRoute`, `newVideoId`, `newChannelId` and other metadata which are to be defined.
---
+++ dist/embed.js
@@ -0,0 +1,164 @@
+/**
+ * @param {string} url
+ * @return {string}
+ */
+function removeHashFromUrl(url) {
+  var match = url.match(/(^.*)?(#.*$)/);
+  return match ? match["1"] : url;
+}
+
+/**
+ * @param {string} url
+ * @return {boolean}
+ */
+function urlHasGetParams(url) {
+  return url.indexOf("?") > -1;
+}
+
+/**
+ * Render CT Frame
+ *
+ * @param {{
+ *    baseUrl: string,
+ *    layout: string,
+ *    exchangeEventName: string,
+ *    wrapperId: string,
+ * } ||* } config
+ * @param {string} ctUrl
+ * @return {CtFrame}
+ * @constructor
+ */
+function CtFrame(config, ctUrl) {
+  if (!config) {
+    throw new Error("Configuration object must be provided.");
+  }
+
+  var _this = this,
+    baseUrl = config.baseUrl,
+    wrapperId = config.wrapperId,
+    layout = config.layout,
+    exchangeEventName = config.exchangeEventName;
+
+  if (!layout || !exchangeEventName || !baseUrl || !wrapperId) {
+    throw new Error("All config params are mandatory. Config: { baseUrl, layout, exchangeEventName, wrapperId}");
+  }
+
+  if (!ctUrl) {
+    throw new Error("CorporateTube Instance url must be provided.");
+  }
+
+  ctUrl = removeHashFromUrl(ctUrl);
+
+  function getCtFramePath() {
+    var re = new RegExp("(^.*" + baseUrl + ")(.*$)", "g"),
+      parsed = re.exec(window.location.href);
+
+    if (parsed === null) {
+      console.error("baseUrl config param isn't correct.");
+    }
+    if ("2" in parsed) {
+      return parsed["2"] === "/" ? "" : parsed["2"];
+    } else {
+      return "";
+    }
+  }
+
+
+  this.getIFrame = function (isoLang) {
+    var iFramePath = removeHashFromUrl(getCtFramePath()),
+      construct = urlHasGetParams(iFramePath) ? "&" : "?",
+      frameUrl = ctUrl + iFramePath + construct +
+        "layout=" + layout +
+        "&exchangeEventName=" + exchangeEventName +
+        "&baseUrl=" + baseUrl +
+        "&lang=" + isoLang || "en";
+
+    return "<iframe src=\"" + frameUrl + "\"/>";
+  };
+
+  this.render = function (isoLang) {
+    document.querySelector("#" + wrapperId).innerHTML = _this.getIFrame(isoLang);
+  };
+
+  return this;
+}
+
+/**
+ *
+ * @param {{
+ *    baseUrl: string,
+ *    layout: string,
+ *    exchangeEventName: string,
+ *    wrapperId: string,
+ * }} configOverwrite
+ * @param {string} ctUrl - Companies CorporateTube Instance url
+ * @default {baseUrl: "/", layout: "regular", exchangeEventName: "on-ct-frame-exchange", wrapperId: "ct-frame-wrapper",}
+ * @return {CtEmbedded}
+ * @constructor
+ */
+function CtEmbedded(configOverwrite, ctUrl) {
+  var observers = [];
+
+  function notifyObservers(event) {
+    var data = event.data;
+
+    if (data.event === CONFIG.exchangeEventName) {
+      observers.map(function (observer) {
+        return observer(data.payload);
+      });
+    }
+  }
+
+  if (!ctUrl) {
+    throw new Error("CorporateTube Instance url must be provided.");
+  }
+  var CONFIG = {
+    baseUrl: configOverwrite.baseUrl || window.location.protocol + "//" + window.location.host,
+    layout: configOverwrite.layout || "iframe",
+    exchangeEventName: configOverwrite.exchangeEventName || "on-ct-frame-exchange",
+    wrapperId: configOverwrite.wrapperId || "ct-frame-wrapper"
+  };
+  var ctFrame = new CtFrame(CONFIG, ctUrl);
+
+  if (!("guessLanguage" in window)) {
+    throw new Error("Missing dependency: https://richtr.github.io/guessLanguage.js");
+  }
+
+  guessLanguage.detect(
+    document.body.innerText,
+    function (isoLang) {
+      ctFrame.render(isoLang);
+    }
+  );
+  window.removeEventListener("message", notifyObservers);
+  window.addEventListener("message", notifyObservers);
+
+  /**
+   * Register observer method to be notified
+   *
+   * @param {Function} observer
+   */
+  this.subscribe = function (observer) {
+    observers.push(observer);
+  };
+
+  /**
+   * Removes all subscribed observers
+   */
+  this.unsubscribe = function () {
+    observers = [];
+  };
+
+  return this;
+}
+
+try {
+  global["isNodeJs"] = true;
+  module.exports = {
+    CtEmbedded: CtEmbedded,
+    CtFrame: CtFrame,
+    removeHashFromUrl: removeHashFromUrl,
+    urlHasGetParams: urlHasGetParams,
+  };
+} catch (e) {/* it is browser*/
+}
